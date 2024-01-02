274498fb98ac51f717f8ad208202553cbd280590
jflanigen <joeyflan@bu.edu>
2024-01-02 11:50:30 -0800

    added support for API
    added functionality for fetching random word with API

---
+++ js-support.js
@@ -0,0 +1,32 @@
+/**
+ * code by Joseph Flanigen
+ * js for word-game
+ * edited: 1-2-24
+ * 
+ */
+
+// Random word API url
+const api_url = 'https://random-word-api.herokuapp.com/word';
+
+function generateWord(){
+    fetch(api_url)
+        .then(response => {
+            if(!response.ok){
+                throw new Error('Response was not ok');
+            }
+            return response.json();
+        })
+        .then(data => {
+            const randomWord = data[0];
+            console.log(randomWord);
+        })
+        .catch(error => {
+            console.error(error);
+        });
+}
+
+generateWord();
+
+
+
+
