372c7c7403560a6b42df79a7956563610f8e065a
jflanigen <joeyflan@bu.edu>
2024-01-03 10:56:22 -0800

    checkUserInput() implementation and adjustments

--- js-support.js
+++ js-support.js
@@ -18,8 +18,15 @@ function generateWord(){
         })
         .then(data => {
             const randomWord = data[0];
-            console.log(randomWord);
-            document.getElementById('currentRandomWord').innerText = randomWord;
+            const currentRandomWord = document.getElementById('currentRandomWord');
+            // turn every character into its own
+            // element for styling/validation purposes
+            randomWord.split('').forEach(char => {
+                const span = document.createElement('span');
+                span.textContent = char;
+                currentRandomWord.appendChild(span);
+            });
+                
         })
         .catch(error => {
             console.error(error);
@@ -27,6 +34,35 @@ function generateWord(){
         });
 }
 
+function checkUserInput(){
+    // get userInput and characters 
+    const userInput = document.getElementById('userInput');
+    const characters = document.getElementById('currentRandomWord').children;
+}
+
+// for start button
+document.addEventListener('keydown', function(event) {
+    if (event.key === 'Enter') {
+        document.getElementById('startGame').click();
+    }
+});
+
+
+// function to run each game session
+function execGame(){
+    // while no mismatch in word typed, used for game status
+    let lettersMatch = true;
+
+    // will be used for WPM calculation
+    let totalNumChars = 0;
+    let timeElapsed = 0.0;
+
+    while(lettersMatch == true){
+        const timerStart = Date.now();
+        currentWord = generateWord();
+        
+    }
+}
 
 
 
