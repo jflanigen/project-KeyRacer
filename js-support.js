25956ca8ee0a2342d5c53e51f47ae969d78a39f5
jflanigen <joeyflan@bu.edu>
2024-01-03 16:17:42 -0800

    attempting to add color changing char
    plus minor changes in html

--- js-support.js
+++ js-support.js
@@ -21,12 +21,13 @@ function generateWord(){
             const currentRandomWord = document.getElementById('currentRandomWord');
             // turn every character into its own
             // element for styling/validation purposes
+            currentRandomWord.innerHTML = '';
             randomWord.split('').forEach(char => {
                 const span = document.createElement('span');
                 span.textContent = char;
                 currentRandomWord.appendChild(span);
             });
-                
+            document.getElementById('userInput').value = '';   
         })
         .catch(error => {
             console.error(error);
@@ -38,14 +39,30 @@ function checkUserInput(){
     // get userInput and characters 
     const userInput = document.getElementById('userInput');
     const characters = document.getElementById('currentRandomWord').children;
+
+    // index through characters of generated word,
+    // change to green if correct (and return true to continue game session),
+    // change to red if incorrect (and return false to terminate game session)
+    for(let i = 0; i < characters.length; i++){
+        const currentChar = characters[i];
+        if(userInput == currentChar){
+            currentChar.style.color = 'green';
+            return true;
+        } else {
+            currentChar.style.color = 'red';
+            return false;
+        }
+    }
 }
 
 // for start button
 document.addEventListener('keydown', function(event) {
     if (event.key === 'Enter') {
         document.getElementById('startGame').click();
+        document.getElementById('userInputButton').click();
     }
 });
+
 
 
 // function to run each game session
@@ -60,7 +77,6 @@ function execGame(){
     while(lettersMatch == true){
         const timerStart = Date.now();
         currentWord = generateWord();
-        
     }
 }
 
