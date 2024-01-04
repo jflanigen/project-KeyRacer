3dc7e016885019f08a82a96b7fa40e67e48a7251
jflanigen <joeyflan@bu.edu>
2024-01-04 11:15:42 -0800

    changing game functionality, execGame()
    Going back to whole word submissions rather than character by character evaluation. Also attempting to add functionality of game within one encompassing function execGame().

--- js-support.js
+++ js-support.js
@@ -8,6 +8,7 @@
 // Random word API url
 const api_url = 'https://random-word-api.herokuapp.com/word';
 
+// generateWord(): fetch random word using random word api 
 function generateWord(){
     fetch(api_url)
         .then(response => {
@@ -18,16 +19,7 @@ function generateWord(){
         })
         .then(data => {
             const randomWord = data[0];
-            const currentRandomWord = document.getElementById('currentRandomWord');
-            // turn every character into its own
-            // element for styling/validation purposes
-            currentRandomWord.innerHTML = '';
-            randomWord.split('').forEach(char => {
-                const span = document.createElement('span');
-                span.textContent = char;
-                currentRandomWord.appendChild(span);
-            });
-            document.getElementById('userInput').value = '';   
+            console.log(randomWord);
         })
         .catch(error => {
             console.error(error);
@@ -35,50 +27,41 @@ function generateWord(){
         });
 }
 
-function checkUserInput(){
-    // get userInput and characters 
-    const userInput = document.getElementById('userInput');
-    const characters = document.getElementById('currentRandomWord').children;
-
-    // index through characters of generated word,
-    // change to green if correct (and return true to continue game session),
-    // change to red if incorrect (and return false to terminate game session)
-    for(let i = 0; i < characters.length; i++){
-        const currentChar = characters[i];
-        if(userInput == currentChar){
-            currentChar.style.color = 'green';
-            return true;
-        } else {
-            currentChar.style.color = 'red';
-            return false;
-        }
+// assembles array wordArray for random words
+function fillArray(array){
+    for(let i = 0; i < array.length; i++){
+        array[i] = generateWord();
     }
+}
+
+function checkInput(userInput, randomWord){
 }
 
 // for start button
 document.addEventListener('keydown', function(event) {
     if (event.key === 'Enter') {
-        document.getElementById('startGame').click();
-        document.getElementById('userInputButton').click();
+        document.getElementById('execGame').click();
+        document.getElementbyId('execGame').disabled = true;
+        document.getElementById('userInput').focus();
     }
 });
 
 
 
-// function to run each game session
+// execGame(): function to run each game session
 function execGame(){
+
+    // initialize randomWords from api
+    randomWords = new Array(20);
+    fillArray(randomWords);
     // while no mismatch in word typed, used for game status
     let lettersMatch = true;
 
     // will be used for WPM calculation
     let totalNumChars = 0;
-    let timeElapsed = 0.0;
+    const startTime = Date.now();
 
     while(lettersMatch == true){
-        const timerStart = Date.now();
-        currentWord = generateWord();
+
     }
 }
-
-
-
