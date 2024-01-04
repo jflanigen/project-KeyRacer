2bd85dc7d3668674996b8eb1b777e44c83772752
jflanigen <joeyflan@bu.edu>
2024-01-04 11:36:05 -0800

    implementing game logic
    Still working on game logic, WPM calculation, accuracy calculation, etc.

--- js-support.js
+++ js-support.js
@@ -34,14 +34,28 @@ function fillArray(array){
     }
 }
 
+// checkInput(): used to manipulate boolean lettersMatch in execGame()
 function checkInput(userInput, randomWord){
+    if(userInput == randomWord){
+        return true;
+    }
+    else {
+        return false;
+    }
 }
 
 // for start button
 document.addEventListener('keydown', function(event) {
     if (event.key === 'Enter') {
+        // click execGame() button to start session
         document.getElementById('execGame').click();
-        document.getElementbyId('execGame').disabled = true;
+
+        // disable execGame button so new sessions are not
+        // started every time enter is pressed
+        document.getElementById('execGame').disabled = true;
+
+        // focus on userInput box.
+        document.getElementById('userInput').click();
         document.getElementById('userInput').focus();
     }
 });
@@ -61,7 +75,30 @@ function execGame(){
     let totalNumChars = 0;
     const startTime = Date.now();
 
-    while(lettersMatch == true){
+    // will be used for accuracy calculation
+    let correctlySolvedWord = 0;
+    let totalNumCharsCorrect = 0;
 
+    // travRWs used to traverse through randomWords
+    let travRWs = 0;
+
+    while(lettersMatch == true && travRWs < 20){
+        if(checkInput == true){
+            totalNumCharsCorrect += randomWords[travRWs].length;
+        } else {
+            // for loop for finding out which characters were correct and which were not from incorrect word
+            for(let i = 0; i < randomWords[travRWs].length; i++){
+                if(randomWords[travRWs].charAt(i) == document.getElementById('userInput').value.charAt(i)){
+                    totalNumCharsCorrect += 1;
+                }
+            }
+            lettersMatch = false();
+        }
+        totalNumChars += randomWords[travRWs].length;
     }
+    
+    // Words per minute calculation
+    let WPM = totalNumChars / (Date.now() - startTime);
+    // Accuracy calculation (% of characters correct)
+    let accuracy = totalNumCharsCorrect / totalNumChars;
 }
