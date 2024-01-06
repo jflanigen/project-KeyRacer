b78032ba66813d560b67d635d06299b58cd1cd5e
jflanigen <joeyflan@bu.edu>
2024-01-06 11:25:45 -0800

    game functionality complete
    Game is complete, functionality-wise. Moving on to finishing the styling then project should be complete at its base

--- js-support.js
+++ js-support.js
@@ -25,6 +25,7 @@ async function generateWord(api_url) {
         }
         const data = await response.json();
         console.log(data);
+        document.getElementById('execGame').style.display = "block";
         return data;
     } catch (error) {
         console.error(error);
@@ -40,6 +41,7 @@ var startTime = 0;
 
 // generated words and traversal
 var randomWords = [];
+var randomWordsCopy = [];
 var userWords = [];
 var currentRandomWord = '';
 var trav = 0;
@@ -67,20 +69,46 @@ function switchToP_EXEC(){
 
 }
 
+// used to perform countdown after pressing start button
+// to give user a buffer between pressing start and needing to type,
+// execGame() will be executed after 'start' button is pressed and timer reaches 0.
+function timer(){
+    document.getElementById('P_EXECGAME').style.display = 'none';
+    document.getElementById('timer').style.display = 'block'
+    document.getElementById('timer').innerText = '3';
+    let countdown = 2;
+    const countdownTimer = setInterval(() => {
+        document.getElementById('timer').innerText = countdown;
+
+        countdown--;
+        if(countdown < 0){
+            clearInterval(countdownTimer);
+            execGame();
+        }
+    }, 1000);
+}
+
 // execGame(): function to run each game session
 function execGame(){
 
-    document.getElementById('P_EXECGAME').style.display = 'none';
+    document.getElementById('timer').style.display = 'none';
     document.getElementById('P_RUNGAME').style.display = 'block';
     document.getElementById('userInput').focus();
 
     // used for WPM calculations
     startTime = Date.now();
+    // copy
+    randomWordsCopy = [... randomWords];
 
     currentRandomWord = randomWords[0];
     document.getElementById('currentRandomWord').innerText = currentRandomWord;
-}
 
+    document.getElementById('userInput').addEventListener('input', function() {
+        if(document.getElementById('userInput').value.length == document.getElementById('currentRandomWord').innerText.length){
+            document.getElementById('submitButton').click();
+        }
+    });
+}
 
 /**
  *  submitUserWord(): Triggered when on P_RUNGAME and enter key is pressed.
@@ -93,47 +121,67 @@ function submitUserWord() {
     let correctWord = document.getElementById('currentRandomWord').innerText;
     // acquire user's submitted word and empty input, store users word in array
     let submittedWord = document.getElementById('userInput').value;
-    document.getElementById('userInput').value = '';
     userWords.push(submittedWord);
-    
-    for(let i = 0; i < correctWord.length; i++){
-        if(submittedWord.charAt(i) == correctWord.charAt(i)){
-            numCharsCorrect += 1;
-        }
-    }
-    if(submittedWord.length < correctWord.length){
-        numChars += correctWord.length;
-    } else {
-        numChars += submittedWord.length;
-    }
-    userCharsTyped = submittedWord.length;
+    submittedWord = "";
+    numChars += correctWord.length;
 
     if(randomWords.length > 1){
         contGame();
     } else {
         endGame();
     }
+    console.log(userWords)
 }
 
-function endGame() {
-    document.getElementById('currentRandomWord').innerText = '';
 
-    let WPM = (userCharsTyped / 5) / ((Date.now() - startTime) / 60000);
+
+function endGame() {
+    document.getElementById('container').style.display = 'none';
+    console.log('user words: ', userWords);
+    console.log('rw: ', randomWordsCopy);
+    for(let j = 0; j < userWords.length; j++){
+        for(let i = 0; i < userWords[j].length; i++){
+            if(userWords[j].charAt(i) === randomWordsCopy[j].charAt(i)){
+                numCharsCorrect += 1;
+            }
+        }
+    }
+
+
     let ACC = numCharsCorrect / numChars;
     let NUM = document.getElementById('numInput').value;
+    let WPM = ((numChars - (numChars - numCharsCorrect))/5) / ((Date.now() - startTime) / 60000);
 
     const ifSingular = n => (n == 1 ? ' word' : ' words');
     document.getElementById('NUM').innerText += NUM + ifSingular(NUM);
-    document.getElementById('WPM').innerText += WPM;
+    document.getElementById('WPM').innerText += Math.round(WPM);
     document.getElementById('ACC').innerText += Math.round((ACC * 100)) + '%';
 
     // display the above values and give restart option.
     document.getElementById('stats').style.display = 'inline-block';
     document.getElementById('restartButton').style.display = 'block';
+
+    // rotate through correct and user submitted words after the game below
+    document.getElementById('displayCorrectWord').innerText = 'Correct Word\n' + randomWordsCopy[0];
+    document.getElementById('displayUserWord').innerText = 'Your Word\n' + userWords[0];
+    let trav = 1;
+    const interval = setInterval(() => {
+        document.getElementById('displayCorrectWord').innerText = 'Correct Word\n' + randomWordsCopy[trav];
+        document.getElementById('displayUserWord').innerText = 'Your Word\n' + userWords[trav];
+
+        trav++;
+        if(trav >= randomWordsCopy.length){
+            trav = 0;
+        }
+    }, 1500);
+
+    
 }
 
 function contGame(){
     randomWords.shift();
     document.getElementById('currentRandomWord').innerText = randomWords[0];
+    document.getElementById('userInput').value = '';
     document.getElementById('userInput').focus();
+
 }
