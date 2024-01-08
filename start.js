0856b99b7dda638ad5a98a649dd8860bf2c5c902
jflanigen <joeyflan@bu.edu>
2024-01-07 20:40:31 -0800

    fix?

--- start.js
+++ start.js
@@ -2,12 +2,12 @@ const express = require("express");
 const path = require('path');
 const app = express();
 
-const PORT = process.env.PORT | 5000;
+const PORT = process.env.PORT || 5000;
 
 app.get('/', (req,res) => {
 	res.sendFile(path.join(__dirname, 'word-game.html'));
 });
 
 app.listen(PORT, () => {
-	console.log('Server running on port ${PORT}');
+	console.log(`Server running on port ${PORT}`);
 });
