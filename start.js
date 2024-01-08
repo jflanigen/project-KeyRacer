f3baa1b8a2b43eb027165f9c9c468f43562e3997
jflanigen <joeyflan@bu.edu>
2024-01-07 20:28:02 -0800

    trying, again.

--- start.js
+++ start.js
@@ -1,7 +1,12 @@
-const express = require("express")
+const express = require("express");
+const path = require('path');
 const app = express();
 
-const PORT = process.env.PORT | 5000;
+app.get('/', (req,res) => {
+	res.sendFile(path.join(__dirname, 'word-game.html'));
+});
 
-const open = require('open');
-open('word-game.html');
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+	console.log('Server running on port ${PORT}');
+});
