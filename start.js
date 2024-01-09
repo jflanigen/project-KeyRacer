9ee6ab51791d813377786fe233a504b9913d24ac
jflanigen <joeyflan@bu.edu>
2024-01-09 11:30:47 -0800

    deployment fix?
    many tries... many errors. all about the learning experience eh ;)

--- start.js
+++ start.js
@@ -3,7 +3,7 @@ const path = require('path');
 const app = express();
 
 app.use(express.static('src'));
-const PORT = process.env.PORT | 5000;
+const PORT = process.env.PORT || 5000;
 
 app.get('/', (req,res) => {
 	res.sendFile(path.join(__dirname, 'word-game.html'));
